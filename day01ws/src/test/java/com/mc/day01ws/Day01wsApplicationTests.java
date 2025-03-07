8f49de77c14bdedabf256fa7346303ed44b6dc42
YUKYUNG <mnew023@gmail.com>
2025-03-07 09:12:49 +0900

    2025.3.7 오후 과제 완료

---
+++ day01ws/src/test/java/com/mc/day01ws/Day01wsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mc.day01ws;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class Day01wsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        log.info("test");
+    }
+
+}
